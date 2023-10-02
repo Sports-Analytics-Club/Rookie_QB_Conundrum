a7f0775f173a995646e7da70273474f555819faf
Eddie Dew <edew@carolina.rr.com>
2023-10-02 18:55:34 -0400

    New stuff HAHA

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Rookie_QB_Conundrum
+# QB Data
+
+This is an example tool used to show and give all QBs who have played in the NFL as well as their html links.
--- scraping_scripts/data.py
+++ scraping_scripts/data.py
@@ -9,27 +9,31 @@ letters = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
 
 # Function to scrape player links for a given letter
 def get_quarterbacks(letter):
-    url = url_template.format(letter)
-    response = requests.get(url)
-    time.sleep(8)
+    try:
 
-    soup = BeautifulSoup(response.text, 'html.parser')
+        url = url_template.format(letter)
+        response = requests.get(url)
+        time.sleep(8)
 
-    players = []
-    for p in soup.select('#div_players p'):
-        meta = p.get_text()
-        position = str(meta.split('(')[1].split(')')[0])
-        is_qb = 1 if 'QB' in position else 0
-        year_span = meta.split(')')[1].strip()
-        start_year = int(year_span.split('-')[0])
-        end_year = int(year_span.split('-')[1])
-        slug = p.find('a')['href']
-        full_slug = f"https://www.pro-football-reference.com{slug}"
-        if is_qb == 1:
-            players.append([meta, position, year_span, start_year, end_year, full_slug])
-    df = pd.DataFrame(players, columns=['meta', 'position', 'year_span', 'start_year', 'end_year', 'full_slug'])
-    df['iter'] = letter # Add the letter for debugging
-    return df
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        players = []
+        for p in soup.select('#div_players p'):
+            meta = p.get_text()
+            position = str(meta.split('(')[1].split(')')[0])
+            is_qb = 1 if 'QB' in position else 0
+            year_span = meta.split(')')[1].strip()
+            start_year = int(year_span.split('-')[0])
+            end_year = int(year_span.split('-')[1])
+            slug = p.find('a')['href']
+            full_slug = f"https://www.pro-football-reference.com{slug}"
+            if is_qb == 1:
+                players.append([meta, position, year_span, start_year, end_year, full_slug])
+        df = pd.DataFrame(players, columns=['meta', 'position', 'year_span', 'start_year', 'end_year', 'full_slug'])
+        df['iter'] = letter # Add the letter for debugging
+        return df
+    except Exception as e:
+        print(f"Error for letter {letter}: {str(e)}")
 
 
 # Scrape all player links for all letters
